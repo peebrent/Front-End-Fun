833e30ce94b9dcab63c97e79c2da9da89079a051
peebrent <paulbrent@gmail.com>
2015-06-14 10:43:04 -0700

    Back

--- starter_code/js/app.js
+++ starter_code/js/app.js
@@ -4,6 +4,11 @@ function Thermostat(){
 
   /* declare variables needed for Thermostat here */
   var currentTemp = 64;
+  var upButton = document.getElementById('up');
+  var downButton = document.getElementById('down');
+  var temperatureText = document.getElementById('temp');
+  var body = document.getElementsByTagName('body')[0];
+
 
   var convertTempToColor = function(temp){
 
@@ -15,17 +20,34 @@ function Thermostat(){
 
   };
 
+  var changeTemperature = function(temp){
+    temperatureText.innerHTML = temp + '<span>&deg;</span>';
+    body.style.backgroundColor = convertTempToColor(temp);
+  };
+
   /* This function initializes the module */
   var init = function(){
 
     console.log('Thermostat is on.');
     console.log('The current temperature is '+currentTemp+'.');
+    changeTemperature(currentTemp);
+
+    upButton.addEventListener('mousedown', function(){
+      currentTemp = currentTemp + 1;
+      changeTemperature(currentTemp);
+
+    });
+
+    downButton.addEventListener('mousedown', function(){
+      currentTemp = currentTemp - 1;
+      changeTemperature(currentTemp);
+
+    });
 
   };
 
   /* This line calls init() function. */
   init();
-
 };
 
 /* This line actually calls the Thermostat module */
