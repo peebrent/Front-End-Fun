f92a08d93ccc5c1c31b9ef09d37c1f44acd6fc02
peebrent <paulbrent@gmail.com>
2015-06-14 18:38:37 -0700

    Comments fixed
    See diff.

--- test code/main2.js
+++ test code/main2.js
@@ -15,33 +15,33 @@ function Thermostat(){
     var hue = -1 * ( (temp - (40*-1)) / (120 - (40*-1)) ) * (360 - 0) + 0;
     return 'hsl('+hue+',70%,60%)';
   };
- // 12. Create this sucker by looking at Steve's code
+ // 11. Create this sucker by looking at Steve's code
   	var changeTemperature = function(temp){
   		temperatureText.innerHTML = temp + '<span>&deg;</span>';
   		body.style.backgroundColor = convertTempToColor(temp);
 
   	};
-// 8. Throw Event Listeners into a function so I can call init
+// 7. Throw Event Listeners into a function so I can call init
 	var init = function(){
 
 	   changeTemperature(currentNum);
 	// 5. Add Event Listener for up Button 
 	   upButton.addEventListener('mousedown', function(){
 	   	currentNum = currentNum + 1;
-	   	// 10. Realize the above alone doesn't work and add below
+	   	// 9. Realize the above alone doesn't work and add below
 	   	changeTemperature(currentNum);
 	   });
 	// 6. Add Event Listener for down Button
 	   downButton.addEventListener('mousedown', function(){
 	   	currentNum = currentNum - 1;
-	   	// 11. For this button as well
+	   	// 10. For this button as well
 	   	changeTemperature(currentNum);
 	   });
 	};
-	// 9. Call init - but why not return the value?
+	// 8. Call init - but why not return the value?
 	init();
 
 };
 
-// 7. I know I'm going to have to call this function so might as well call and console.log
+// 2. I know I'm going to have to call this function so might as well call and console.log
 Thermostat();
