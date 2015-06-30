b9a2b06d54939830e159fb3f63e3305187413d20
peebrent <paulbrent@gmail.com>
2015-06-30 14:02:41 -0700

    Comments
    Still need to code this out - time permitting

--- gallery-2/js/app.js
+++ gallery-2/js/app.js
@@ -10,3 +10,6 @@ galleryElement.Gallery();
 
 var contactElement = document.getElementById('contact');
 contactElement.Contact();
+
+var searchElement = document.getElementById('search');
+searchElement.Search();
---
+++ gallery-2/js/search.js
@@ -0,0 +1,15 @@
+Element.prototype.Search = function(){
+
+var gallery = document.getElementById('gallery');
+//gallery.getFilteredLis() or whatever we call it
+//also want to use similar something to li.dataset.description = photo.description;
+//also set the tags as data attributes
+//maybe all should result in all photos
+//data tags is a useful attribute
+// log the photo variable and find where the tags are - steve recommend
+//I added to the app on my own.  make lis show and hide based on input of the search
+
+//when the user focuses on the input, clear its contents
+//after the user presses enter or return, filter the gallery <li> using tags from the JSON model
+
+};
