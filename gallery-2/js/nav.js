4f4f15b10c3bf65e0de1b4c5ade72c5da3a88757
peebrent <paulbrent@gmail.com>
2015-06-29 20:08:12 -0700

    Gallery Toggle
    Feature added

--- gallery-2/js/nav.js
+++ gallery-2/js/nav.js
@@ -1,8 +1,13 @@
 Element.prototype.Nav = function(){
 
   var nav = this,
+      navItems = nav.children[0].children,
+       //above gives us a node list
+      sections = document.getElementsByTagName('section'),
       btn = document.createElement('div'),
       container = document.getElementById('container');
+
+    
 
   this.toggleNav = function(){
     if(container.style.left === "0px"){
@@ -22,11 +27,43 @@ Element.prototype.Nav = function(){
     container.appendChild(btn);
 
   };
+  // hide all the sections
+  this.hideSections = function(){
+    for(var i=0;i<sections.length;i++){
+      sections[i].style.opacity = '0';
+      sections[i].style.zIndex = '0';
+      sections[i].style.display = 'none';
+    };
+  };
+
+  // show a section
+  this.showSection = function(id){
+    this.hideSections();  
+    document.getElementById(id).style.display = 'block';
+    document.getElementById(id).style.opacity = '1.0';
+    document.getElementById(id).style.zIndex = '50';
+  };
+
+  // hide all the sections - order here important
+  // show a section
+
 
   this.init = function(){
 
     nav.createButton();
 
+    //loop through all li and add event listener
+
+    for (var i=0;i<navItems.length;i++){
+      navItems[i].addEventListener('click', function(ev){
+        var id = ev.target.dataset.section;
+        nav.showSection(id);
+        nav.toggleNav();
+
+       
+      });
+   
+    }
   };
 
   this.init();
